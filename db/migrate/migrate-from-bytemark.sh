b8344777357f1b3f8ac9fd63506650f3d78feb2a
Tim Sawyer <tsawyer@brassbandresults.co.uk>
2018-07-28 18:28:36 +0100

    More migration script development

--- db/migrate/migrate-from-bytemark.sh
+++ db/migrate/migrate-from-bytemark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sed -i 's/OWNER TO postgres/OWNER TO rds_superuser/g' ~/bbr.sql
+# expects a file in ~/bbr.sql which was exported with:
+#  pg_dump bbr --no-owner --quote-all-identifiers --inserts > bbr-migrate.sql
 
 psql -h bbr-db.csxjs9r4u4br.eu-west-2.rds.amazonaws.com -U bbradmin -d bbr -f prepare.sql
-psql -h bbr-db.csxjs9r4u4br.eu-west-2.rds.amazonaws.com -U bbradmin -d bbr -v ON_ERROR_STOP=1 -f ~/bbr.sql
+psql -h bbr-db.csxjs9r4u4br.eu-west-2.rds.amazonaws.com -U bbr -d bbr -f ~/bbr.sql
 
