ead2fd5f0201b58f6500d4ab5f57669280eb158b
Tim Sawyer <tsawyer@brassbandresults.co.uk>
2018-07-30 18:42:42 +0100

    Moved terraform state to s3 Added to database migration script

--- terraform/config.tf
+++ terraform/config.tf
@@ -3,3 +3,11 @@ provider "aws" {
     profile = "default"
     region = "${var.region}"
 }
+
+terraform {
+    backend "s3" {
+        bucket = "bbr-terraform-state"
+        key = "tfstate"
+        region = "eu-west-2" 
+    }
+}
