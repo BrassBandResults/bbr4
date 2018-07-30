cea933056e1f6313ffbe118cb14d025d260e0f9c
Tim Sawyer <tsawyer@brassbandresults.co.uk>
2018-07-30 20:27:24 +0100

    Improved migration script Corrected wsgi file Added gunicorn to requirements

--- db/migrate/migrate-from-bytemark.sh
+++ db/migrate/migrate-from-bytemark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # expects a file in ~/bbr.sql which was exported with:
-#  pg_dump bbr --no-owner --quote-all-identifiers --inserts > bbr-migrate.sql
+#  pg_dump bbr --no-owner --quote-all-identifiers --inserts > bbr.sql
 
 psql -h bbr-db.csxjs9r4u4br.eu-west-2.rds.amazonaws.com -U bbradmin -d bbr -f prepare.sql
 psql -h bbr-db.csxjs9r4u4br.eu-west-2.rds.amazonaws.com -U bbr -d bbr -f ~/bbr.sql
--- web/site/bbr/wsgi.py
+++ web/site/bbr/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bbr4.settingslive")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bbr.settingslive")
 
 application = get_wsgi_application()
