63080aac5ab45dfb93f44485e42e0540722e22d4
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-08-15 21:56:52 +0100

    Try notification message with default at root level

--- web/site/notification/tests.py
+++ web/site/notification/tests.py
@@ -4,6 +4,9 @@ from django.test import TestCase
 from notification.notify import MessageWrapper
 
 class MockClient:
+    """
+    Mock boto3 style client mirroring SNS interface
+    """
     messages = None
 
     def __init__(self):
@@ -14,8 +17,11 @@ class MockClient:
 
 
 class MessageWrapperTestCase(TestCase):
-    def testSendAllNone(self):
 
+    def testSendAllNone(self):
+        """
+        Send with all values passed in as None
+        """
         lExpectedMessage = """{"notification": {
   "module" : "None",
   "type" : "None",
@@ -31,7 +37,6 @@ class MessageWrapperTestCase(TestCase):
   "thingOld" : null,
   "thingNew" : null
 }"""
-
 
         lTestMessage = MessageWrapper(
             pThingOld = None, 
@@ -49,6 +54,49 @@ class MessageWrapperTestCase(TestCase):
             pFromEmail = None,
             )
         lMockClient = MockClient()
+        self.assertEqual(len(lMockClient.messages), 0)
+        lTestMessage.send(lMockClient)
+        self.assertEqual(len(lMockClient.messages), 1)
+        self.assertEqual(lExpectedMessage, lMockClient.messages[0])
+
+
+    def testSendNoThings(self):
+        """
+        Test send with all values filled in except things
+        """
+        lExpectedMessage = """{"notification": {
+  "module" : "contests",
+  "type" : "contest",
+  "change" : "edit",
+  "user" : "test",
+  "ip" : "127.0.0.1",
+  "browser" : "Netscape Navigator",
+  "destination" : "destination@brassbandresults.co.uk",
+  "cc" : "cc@brassbandresults.co.uk",
+  "bcc" : "bcc@brassbandresults.co.uk",
+  "fromName" : "Tim Sawyer",
+  "fromEmail" : "source@brassbandresults.co.uk",
+  "thingOld" : null,
+  "thingNew" : null
+}"""
+
+        lTestMessage = MessageWrapper(
+            pThingOld = None, 
+            pThingNew = None, 
+            pModule = "contests", 
+            pObjectType = "contest", 
+            pChangeType = "edit", 
+            pUser = "test", 
+            pBrowserDetails = ("127.0.0.1", "Netscape Navigator"), 
+            pDestination = "destination@brassbandresults.co.uk", 
+            pAdditionalContext = None, 
+            pCc = "cc@brassbandresults.co.uk", 
+            pBcc = "bcc@brassbandresults.co.uk", 
+            pFromName = "Tim Sawyer", 
+            pFromEmail = "source@brassbandresults.co.uk",
+            )
+        lMockClient = MockClient()
+        self.assertEqual(len(lMockClient.messages), 0)
         lTestMessage.send(lMockClient)
         self.assertEqual(len(lMockClient.messages), 1)
         self.assertEqual(lExpectedMessage, lMockClient.messages[0])
