f382b06e043b88d20fc402c792da5fd9f824e510
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-08-19 17:08:29 +0100

    Thumbnails tag

--- web/site/home/templatetags/thumbnail.py
+++ web/site/home/templatetags/thumbnail.py
@@ -2,9 +2,10 @@ from django.utils.safestring import mark_safe
 from django.conf import settings
 from django import template
 
-def thumbnail(pathToImage, dimensions):
-    lExtension = pathToImage[pathToImage.rfind('.'):]
-    lThumbPath = "%s%s-%s%s" % (settings.THUMB_URL, pathToImage, dimensions, lExtension)
+def thumbnail(imageFieldFile, dimensions):
+    lPathToImage = imageFieldFile.image_url.url
+    lExtension = lPathToImage[lPathToImage.rfind('.'):]
+    lThumbPath = "%s%s-%s%s" % (settings.THUMB_URL, lPathToImage, dimensions, lExtension)
     return mark_safe("<img src='%s'/>" % lThumbPath)
 
 thumbnail.is_safe=True
