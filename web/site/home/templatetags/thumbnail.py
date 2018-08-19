0596f486c29890f2d4495a1fe64f7a78dee0c9c4
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-08-19 17:01:14 +0100

    Initial code from new thumbnail template tag

---
+++ web/site/home/templatetags/thumbnail.py
@@ -0,0 +1,12 @@
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
+def thumbnail(pathToImage, dimensions):
+    lExtension = pathToImage[pathToImage.rfind('.'):]
+    lThumbPath = "%s%s-%s%s" % (settings.THUMB_URL, pathToImage, dimensions, lExtension)
+    return mark_safe("<img src='%s'/>" % lThumbPath)
+
+thumbnail.is_safe=True
+thumbnail.needs_autoescape = False
+
+register.simple_tag(thumbnail)
