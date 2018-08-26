89a1e604f5e2ef8b80431d2d21579470dda6b958
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-08-26 18:31:11 +0100

    Link through to larger image from programme thumbnail

--- web/site/home/templatetags/thumbnail.py
+++ web/site/home/templatetags/thumbnail.py
@@ -2,10 +2,14 @@ from django.utils.safestring import mark_safe
 from django.conf import settings
 from django import template
 
-def thumbnail(imageFieldFile, dimensions):
+def thumbnail_path(imageFieldFile, dimensions):
     lPathToImage = imageFieldFile.url
     lExtension = lPathToImage[lPathToImage.rfind('.'):]
     lThumbPath = "%s%s-%s%s" % (settings.THUMBS_URL, lPathToImage, dimensions, lExtension)
+    return lThumbPath
+
+def thumbnail(imageFieldFile, dimensions):
+    lThumbPath = thumbnail_path(imageFieldFile, dimensions)
     return mark_safe("<img src='%s'/>" % lThumbPath)
 
 thumbnail.is_safe=True
@@ -13,3 +17,4 @@ thumbnail.needs_autoescape = False
 
 register = template.Library()
 register.simple_tag(thumbnail)
+register.simple_tag(thumbnail_path)
