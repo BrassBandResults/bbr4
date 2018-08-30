3440a737641422a628e4b89b372e0d9858448860
Tim Sawyer <tim.git@hotmandarins.co.uk>
2018-08-30 10:03:23 +0000

    Mark filter results as safe

--- web/site/home/templatetags/quote_filter.py
+++ web/site/home/templatetags/quote_filter.py
@@ -1,7 +1,8 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter(name='escapejsonquote')
 def escape_json_quote(value):
-  return value.replace('\\"','\\\"')
+  return mark_safe(value.replace('\\"','\\\"'))
