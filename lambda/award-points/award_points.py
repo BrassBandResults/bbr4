1c5e462237f125e5e9187587e85f8f19005b6776
Tim Sawyer <tim.git@hotmandarins.co.uk>
2018-09-04 09:52:42 +0000

    Development of points award function

--- lambda/award-points/award_points.py
+++ lambda/award-points/award_points.py
@@ -3,12 +3,27 @@
 import boto3
 import json
 
+def _moved(notification):
+  """
+  Return true if location of band in ThingNew differs from the one in ThingOld.
+  """
+  thingOld = notification["thingOld"][0]["fields"]
+  thingNew = notification["thingNew"][0]["fields"]
+
+  isLocationChanged = False
+  if (thingOld["latitude"] != thingNew["latitude"]):
+    isLocationChanged = True
+  if (thingOld["longitude"] != thingNew["longitude"]):
+    isLocationChanged = True
+
+  return isLocationChanged
+
 
 POINTS = {
   # New result 1 point
-  # Map move 5 points
-  # Feedback claim 5 points
-  # Venue map 5 points
+  "feedback.feedback.claim" : 5,
+  "feedback.feedback.to_queue": -5,
+  "venues.venue_map.move" : 5,
   "bands.band_map.move" : 5,
 }
 
@@ -28,12 +43,20 @@ def lambda_handler(event, context):
   notifyContextPath = "%s.%s.%s" % (notifyModule, notifyType, notifyChange)
   
   print("Looking for notification path %s" % notifyContextPath)
-  
-  pointsToAdd = 0
+
+  if ('bands.band.edit' == notifyContextPath && _moved(parsedMessage["notification"])):
+    # band edited with location move, so make sure to add points
+    notifyContextPath = 'bands.band_map.move'  
+ 
+  if ('venues.venue.edit' == notifyContextPath && _moved(parsedMessage["notification"])):
+    # venue edited with location move
+    notifyContextPath = "venues.venue_map.move'
+ 
   try:
     pointsToAdd  = POINTS[notifyContextPath]
   except KeyError:
-    pass 
-  if pointsToAdd:
+    pointsToAdd = 0 
+  
+if pointsToAdd:
     print("Adding %s points to user %s" % (pointsToAdd, userToAddTo))
 
