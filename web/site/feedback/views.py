9015fa189630de8f5c6d943201a971a56c3a3098
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-12-16 13:18:25 +0000

    Fix feedback notification

--- web/site/feedback/views.py
+++ web/site/feedback/views.py
@@ -81,7 +81,7 @@ def feedback(request):
                         'Url' : lUrl,
                         'Referrer' : lReferrer,
                         }
-            notification(None, lFeedback, 'feedback', 'feedback', 'new', request.user, browser_details(request), pDestination=lOwnerEmail, pAdditionalContext=lContext, pUser=lUrl)
+            notification(None, lFeedback, 'feedback', 'feedback', 'new', request.user, browser_details(request), pDestination=lOwnerEmail, pAdditionalContext=lContext, pUrl=lUrl)
                 
         lNextUrl = lUrl[len('http://'):]
         lNextUrl = lNextUrl[lNextUrl.find('/'):]
