cfef91685f9cd9d426a0c40681cd78d7d09dcfa4
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-12-28 17:47:29 +0000

    Remove db migration code

--- db/migrate/prepare.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION postgis;
-CREATE EXTENSION postgis;
-CREATE EXTENSION fuzzystrmatch;
-CREATE EXTENSION postgis_tiger_geocoder;
-CREATE EXTENSION postgis_topology;
-
-ALTER SCHEMA topology owner to rds_superuser;
-
-CREATE USER bbr WITH PASSWORD '045003a0083366dea';
