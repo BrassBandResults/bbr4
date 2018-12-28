08073a2f61dc6fe1a3c428b5c76f97daf645d6ef
Tim Sawyer <tim.git@hotmandrins.co.uk>
2018-12-28 20:36:25 +0000

    Fix imports

--- web/site/bands/views.py
+++ web/site/bands/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.db import connection
 from django.db.models.query_utils import Q
 from django.template.loader import render_to_string
