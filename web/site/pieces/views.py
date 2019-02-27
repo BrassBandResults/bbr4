229dfee335d04061a3c158fdc02018686f62a8c9
Tim Sawyer <tim.git@hotmandrins.co.uk>
2019-02-27 09:33:42 +0000

    Support piece history with results on same date

--- web/site/pieces/views.py
+++ web/site/pieces/views.py
@@ -23,7 +23,7 @@ def _fetch_piece_counts(pSectionId=None):
     """
     Return two dictionaries, one for own choice pieces (id->count) and the other for set tests, limited to contests in the specified section if specified
     """
-    
+
     # find own choice piece performances
     own_choice_cursor = connection.cursor()
     lOwnChoiceResults = {}
@@ -37,7 +37,7 @@ def _fetch_piece_counts(pSectionId=None):
     for row in simple_rows:
         lOwnChoiceResults[row[0]] = row[1]
     own_choice_cursor.close()
-    
+
     # find set test piece performances
     simple_cursor = connection.cursor()
     extra_cursor = connection.cursor()
@@ -61,20 +61,20 @@ def _fetch_piece_counts(pSectionId=None):
             lExistingCount = lSetTestResults[row[0]]
         except KeyError:
             lExistingCount = 0
-        lSetTestResults[row[0]] = lExistingCount + row[1] 
+        lSetTestResults[row[0]] = lExistingCount + row[1]
     simple_cursor.close()
     extra_cursor.close()
-    
+
     # find entertainments piece performances
     entertainments_cursor = connection.cursor()
     if pSectionId == None:
         lSetTestSql = "SELECT piece_id, count(*) FROM contests_resultpieceperformance WHERE piece_id is not null group by piece_id"
         entertainments_cursor.execute(lSetTestSql)
     else:
-        lSetTestSql = """SELECT piece_id, count(*) 
+        lSetTestSql = """SELECT piece_id, count(*)
                         FROM contests_resultpieceperformance p
                         INNER JOIN contests_contestresult r ON (p.result_id = r.id)
-                        WHERE r.contest_event_id in (SELECT e.id FROM contests_contestevent e, contests_contest c WHERE e.contest_id = c.id AND c.section_id =  %(section)s) 
+                        WHERE r.contest_event_id in (SELECT e.id FROM contests_contestevent e, contests_contest c WHERE e.contest_id = c.id AND c.section_id =  %(section)s)
                         GROUP BY piece_id"""
         entertainments_cursor.execute(lSetTestSql, {'section' : pSectionId})
     ents_rows = entertainments_cursor.fetchall()
@@ -83,9 +83,9 @@ def _fetch_piece_counts(pSectionId=None):
             lExistingCount = lOwnChoiceResults[row[0]]
         except KeyError:
             lExistingCount = 0
-        lOwnChoiceResults[row[0]] = lExistingCount + row[1] 
+        lOwnChoiceResults[row[0]] = lExistingCount + row[1]
     entertainments_cursor.close()
-    
+
     return lSetTestResults, lOwnChoiceResults
 
 def piece_list(request):
@@ -93,13 +93,13 @@ def piece_list(request):
     Show list of pieces starting with A
     """
     return piece_list_filter_letter(request, 'A')
-    
+
 def piece_list_filter_letter(request, pLetter):
     """
     Show list of pieces that start with specified letter
     """
     lSetTestResults, lOwnChoiceResults = _fetch_piece_counts()
-    
+
     lPieceQuery = TestPiece.objects.all().select_related('composer', 'arranger')
     lAliasQuery = TestPieceAlias.objects.all().select_related()
     if pLetter == 'ALL':
@@ -117,8 +117,8 @@ def piece_list_filter_letter(request, pLetter):
             piece.own_choice_count = lOwnChoiceResults[piece.id]
         except KeyError:
             piece.own_choice_count = 0
-           
-        try:    
+
+        try:
             piece.set_test_count = lSetTestResults[piece.id]
         except KeyError:
             piece.set_test_count = 0
@@ -127,8 +127,8 @@ def piece_list_filter_letter(request, pLetter):
             alias.own_choice_count = lOwnChoiceResults[alias.piece_id]
         except KeyError:
             alias.own_choice_count = 0
-           
-        try:    
+
+        try:
             alias.set_test_count = lSetTestResults[alias.piece_id]
         except KeyError:
             alias.set_test_count = 0
@@ -138,8 +138,8 @@ def piece_list_filter_letter(request, pLetter):
                                                        "ResultCount" : len(lPieces),
                                                        "PieceCount" : lPieceCount,
                                                        "StartsWith" : pLetter})
-    
-    
+
+
 def single_piece(request, pPieceSlug):
     """
     Show a single test piece
@@ -148,7 +148,7 @@ def single_piece(request, pPieceSlug):
         lPiece = TestPiece.objects.filter(slug=pPieceSlug).select_related('composer', 'arranger')[0]
     except IndexError:
         raise Http404()
-    
+
     # find list of contest event ids where this piece was an extra set test
     lContestEventIdsWhereSetTest = []
     lContestEventIdsWhereSetTestAnd = []
@@ -161,7 +161,7 @@ def single_piece(request, pPieceSlug):
         if row[1] == 'and':
             lContestEventIdsWhereSetTestAnd.append(row[0])
     cursor.close()
-    
+
     # find this user's playing of this piece from their contest history
     lUserResultsForThisPiece = []
     if request.user.is_anonymous() == False:
@@ -172,18 +172,20 @@ def single_piece(request, pPieceSlug):
                 lUserResultsForThisPieceWithDate.append((lUserHistory.result.contest_event.date_of_event, lUserHistory.result))
         lUserOwnChoiceThisPiece = PersonalContestHistory.objects.filter(user=request.user, status='accepted', result__test_piece=lPiece).select_related('result', 'result__band', 'result__contest_event', 'result__contest_event__contest')
         if len(lUserOwnChoiceThisPiece):
+            index = 1 #  without this it fails to sort for two results on the same date
             for lUserOwnChoiceHistory in lUserOwnChoiceThisPiece:
-                lUserResultsForThisPieceWithDate.append((lUserOwnChoiceHistory.result.contest_event.date_of_event, lUserOwnChoiceHistory.result))
+                lUserResultsForThisPieceWithDate.append((lUserOwnChoiceHistory.result.contest_event.date_of_event, index, lUserOwnChoiceHistory.result))
+                index += 1
         lUserResultsForThisPieceWithDate.sort()
         lUserResultsForThisPieceWithDate.reverse()
-        
+
         for result in lUserResultsForThisPieceWithDate:
             lUserResultsForThisPiece.append(result[1])
-            
+
         lContestHistorysWithAdditionalTestPiece = PersonalContestHistory.objects.filter(user=request.user, status='accepted', result__contest_event__id__in=lContestEventIdsWhereSetTestAnd)
         for result in lContestHistorysWithAdditionalTestPiece:
             lUserResultsForThisPiece.append(result.result)
-    
+
     # find list of contest event ids where this piece used as set test
     lSetTestSql = "SELECT id FROM contests_contestevent WHERE test_piece_id = %(piece_id)s"
     cursor = connection.cursor()
@@ -192,20 +194,20 @@ def single_piece(request, pPieceSlug):
     for row in rows:
         lContestEventIdsWhereSetTest.append(row[0])
     cursor.close()
-    
+
     # find all the contest events objects given both sets of ids
     lUsageSetTest = ContestEvent.objects.filter(id__in=lContestEventIdsWhereSetTest).select_related('contest')
-    
+
     if len(lContestEventIdsWhereSetTest) > 0:
         # work out who won these set test piece contests
         lIdsString = str(tuple(lContestEventIdsWhereSetTest))
         lIdsStringLessBrackets = lIdsString[1:-1].strip()
         if lIdsStringLessBrackets.endswith(','):
             lIdsStringLessBrackets = lIdsStringLessBrackets[:-1]
-        lWinnersSetTestSql = """SELECT e.id, r.band_name, b.name, b.slug 
-                                FROM contests_contestresult r, bands_band b, contests_contestevent e 
-                                WHERE e.id IN (%s) 
-                                AND b.id = r.band_id 
+        lWinnersSetTestSql = """SELECT e.id, r.band_name, b.name, b.slug
+                                FROM contests_contestresult r, bands_band b, contests_contestevent e
+                                WHERE e.id IN (%s)
+                                AND b.id = r.band_id
                                 AND r.contest_event_id = e.id
                                 AND r.results_position = 1""" % lIdsStringLessBrackets
         cursor = connection.cursor()
@@ -222,27 +224,27 @@ def single_piece(request, pPieceSlug):
                     event.current_band_name = lCurrentBandName
                     event.band_slug = lBandSlug
         cursor.close()
-    
-    
+
+
     # find out results where this piece was used as an own choice test piece
     lUsageOwnChoice = ContestResult.objects.filter(test_piece=lPiece).select_related('band','contest_event', 'contest_event__contest')
     lShowEdit = False
     if request.user.is_anonymous() == False:
         lShowEdit = request.user.profile.superuser or (request.user.profile.enhanced_functionality and request.user.id == lPiece.owner.id)
-        
+
     lUsageEntertainments = ResultPiecePerformance.objects.filter(piece=lPiece).select_related('result')
     lUsageOwnChoiceAndEnts = []
     for own_choice in lUsageOwnChoice:
         lUsageOwnChoiceAndEnts.append(own_choice)
     for ents in lUsageEntertainments:
         lUsageOwnChoiceAndEnts.append(ents.result)
-        
+
     lDownloadTracks = DownloadTrack.objects.filter(test_piece=lPiece).select_related('album', 'album__store')
     lDownloadAlbums = {}
     for track in lDownloadTracks:
         lDownloadAlbums[track.album.id] = track.album
     lDownloadAlbums = lDownloadAlbums.values()
-    
+
     # Set test data for chart
     lSetTestYearCount = {}
     for event in lUsageSetTest:
@@ -252,14 +254,14 @@ def single_piece(request, pPieceSlug):
             lCount = 0
         lCount += 1
         lSetTestYearCount[event.date_of_event.year] = lCount
-        
+
     lSetTestChartData = ""
     lYear = lSetTestYearCount.keys()
     sorted(lYear)
     for year in lYear:
         lYearCount = lSetTestYearCount[year]
-        lSetTestChartData += "['%d', %d]," % (year, lYearCount) 
-        
+        lSetTestChartData += "['%d', %d]," % (year, lYearCount)
+
     # Own choice data for chart
     lOwnChoiceYearCount = {}
     for result in lUsageOwnChoiceAndEnts:
@@ -269,14 +271,14 @@ def single_piece(request, pPieceSlug):
             lCount = 0
         lCount += 1
         lOwnChoiceYearCount[result.contest_event.date_of_event.year] = lCount
-        
+
     lOwnChoiceChartData = ""
     lYear = lOwnChoiceYearCount.keys()
     sorted(lYear)
     for year in lYear:
         lYearCount = lOwnChoiceYearCount[year]
-        lOwnChoiceChartData += "['%d', %d]," % (year, lYearCount) 
-        
+        lOwnChoiceChartData += "['%d', %d]," % (year, lYearCount)
+
     return render_auth(request, 'pieces/piece.html', {"Piece" : lPiece,
                                                       "UsageSetTest" : lUsageSetTest,
                                                       "UsageOwnChoice" : lUsageOwnChoiceAndEnts,
@@ -325,7 +327,7 @@ def _get_pieces_for_section(pSectionName):
                 'piece_composer':'SELECT composer_id FROM pieces_testpiece WHERE id = contests_contestevent.test_piece_id',
                 'piece_arranger':'SELECT arranger_id FROM pieces_testpiece WHERE id = contests_contestevent.test_piece_id',
                 }).select_related()
-    lComposerArranger = {}                
+    lComposerArranger = {}
     for contest_event in lContestEvents:
         if contest_event.piece_name:
             lTestPiece = TestPiece()
@@ -340,14 +342,14 @@ def _get_pieces_for_section(pSectionName):
                 lTestPiece.own_choice_count = lOwnChoiceResults[lTestPiece.id]
             except KeyError:
                 lTestPiece.own_choice_count = 0
-           
-            try:    
+
+            try:
                 lTestPiece.set_test_count = lSetTestResults[lTestPiece.id]
             except KeyError:
                 lTestPiece.set_test_count = 0
-            
-            lPieces[contest_event.piece_name] = lTestPiece 
-    
+
+            lPieces[contest_event.piece_name] = lTestPiece
+
     lComposers = Person.objects.filter(id__in=lComposerArranger.keys())
     for lPiece in lPieces.values():
         for lComposer in lComposers:
@@ -355,10 +357,10 @@ def _get_pieces_for_section(pSectionName):
                 lPiece.composer = lComposer
             if lComposer.id == lPiece.arranger_id:
                 lPiece.arranger = lComposer
-                
+
     return lPieces.values()
 
-@login_required  
+@login_required
 def add_piece(request):
     """
     Add a new test piece
@@ -374,15 +376,15 @@ def add_piece(request):
             lNewPiece.lastChangedBy = request.user
             lNewPiece.owner = request.user
             lNewPiece.save()
-            notification(None, lNewPiece, 'pieces', 'piece', 'new', request.user, browser_details(request))            
+            notification(None, lNewPiece, 'pieces', 'piece', 'new', request.user, browser_details(request))
             return HttpResponseRedirect('/pieces/')
     else:
         form = EditPieceForm()
 
     return render_auth(request, 'pieces/new_piece.html', {'form': form})
-    
-    
-@login_required  
+
+
+@login_required
 def edit_piece(request, pPieceSlug):
     """
     Edit a test piece
@@ -393,7 +395,7 @@ def edit_piece(request, pPieceSlug):
         raise Http404()
     if request.user.profile.superuser == False:
         if request.user.id != lPiece.owner.id:
-            raise Http404() 
+            raise Http404()
         if request.user.profile.enhanced_functionality == False:
             raise Http404()
     if request.method == 'POST':
@@ -408,7 +410,7 @@ def edit_piece(request, pPieceSlug):
     else:
         form = EditPieceForm(instance=lPiece)
 
-    return render_auth(request, 'pieces/edit_piece.html', {'form': form, "Piece" : lPiece})    
+    return render_auth(request, 'pieces/edit_piece.html', {'form': form, "Piece" : lPiece})
 
 @login_required
 def piece_options(request):
@@ -435,9 +437,9 @@ def best_own_choice(request):
     lPieceNames = {}
     cursor = connection.cursor()
     lOwnChoiceSql = """
-       SELECT r.results_position, p.slug, p.name 
+       SELECT r.results_position, p.slug, p.name
        FROM contests_contestresult r
-       INNER JOIN pieces_testpiece p ON r.test_piece_id = p.id  
+       INNER JOIN pieces_testpiece p ON r.test_piece_id = p.id
        INNER JOIN contests_contestevent e ON e.id = r.contest_event_id
        INNER JOIN contests_contest c ON c.id = e.contest_id
        WHERE r.results_position < 4
@@ -457,13 +459,13 @@ def best_own_choice(request):
         lNewCount = lExistingData[1] + 1
         lBestOwnChoice[lPieceSlug] = (lNewScore, lNewCount)
     cursor.close()
-    
+
     lPointsTuples = []
     for slug, points in lBestOwnChoice.items():
         lPointsTuples.append((points, slug, lPieceNames[slug]))
     lPointsTuples.sort()
     lPointsTuples.reverse()
-    
+
     # lPointsTuples in now in order
     lPieces = []
     for row in lPointsTuples:
@@ -473,6 +475,5 @@ def best_own_choice(request):
         lPiece.slug = row[1]
         lPiece.name = row[2]
         lPieces.append(lPiece)
-    
-    return render_auth(request, 'pieces/best_own_choice.html', {'Pieces' : lPieces})
 
+    return render_auth(request, 'pieces/best_own_choice.html', {'Pieces' : lPieces})
