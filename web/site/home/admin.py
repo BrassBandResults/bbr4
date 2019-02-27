59340551fed3293c5f716543e4da5b8b3db5faaf
Tim Sawyer <tim.git@hotmandrins.co.uk>
2019-02-27 15:33:40 +0000

    FAQ in admin site

--- web/site/home/admin.py
+++ web/site/home/admin.py
@@ -1,3 +1,16 @@
+# (c) 2009, 2012, 2015, 2017, 2019 Tim Sawyer, All Rights Reserved
+
 from django.contrib import admin
 
-# Register your models here.
+from bbr.admin import BbrAdmin
+from home.models import FaqSection, FaqEntry
+
+
+class FaqSectionAdmin(BbrAdmin):
+    pass
+
+class FaqEntryAdmin(BbrAdmin):
+    pass
+
+admin.site.register(FaqSection, FaqSectionAdmin)
+admin.site.register(FaqEntry, FaqEntryAdmin)
