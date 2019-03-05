254a62a406aa66cd17e173029ea48bd189b0323f
Tim Sawyer <tim.git@hotmandrins.co.uk>
2019-03-05 21:15:15 +0000

    Add Contest Adjudicators to Admin site

--- web/site/adjudicators/admin.py
+++ web/site/adjudicators/admin.py
@@ -1,1 +1,10 @@
-# (c) 2009, 2012, 2015, 2017, 2018 Tim Sawyer, All Rights Reserved
+# (c) 2009, 2012, 2015, 2017, 2018, 2019 Tim Sawyer, All Rights Reserved
+
+from django.contrib import admin
+from bbr.admin import BbrAdmin
+from adjudicators.models import ContestAdjudicator
+
+class ContestAdjudicatorAdmin(BbrAdmin):
+    pass
+
+admin.site.register(ContestAdjudicator, ContestAdjudicatorAdmin)
