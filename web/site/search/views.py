f0e50f97516940ef6397b82ab98463718e7a6528
Tim Sawyer <tim.git@hotmandrins.co.uk>
2019-03-24 21:38:19 +0000

    Correct delete link on search results

--- web/site/search/views.py
+++ web/site/search/views.py
@@ -44,9 +44,11 @@ class ResultWrapper(object):
         try:
             lContentType = ContentType.objects.get_for_model(pObjectToWrapper.__class__)
             self.admin_offset = urlresolvers.reverse("admin:%s_%s_change" % (lContentType.app_label, lContentType.model), args=(pObjectToWrapper.id,))
+            self.delete_offset = urlresolvers.reverse("admin:%s_%s_delete" % (lContentType.app_label, lContentType.model), args=(pObjectToWrapper.id,))
         except NoReverseMatch:
             self.admin_offset = None
-        
+            self.delete_offset = None
+
 class PreviousBandWrapper(object):
     """
     Wrapper class for previous band names
@@ -58,8 +60,8 @@ class PreviousBandWrapper(object):
         self.name = pPreviousBandNameObject.old_name
         self.type = "Alternative name for %s" % pPreviousBandNameObject.band.name
         self.title = pPreviousBandNameObject.band.name
-        
-        
+
+
 class PersonAliasWrapper(object):
     """
     Wrapper class for person aliases
@@ -69,9 +71,9 @@ class PersonAliasWrapper(object):
         self.url = "people"
         self.slug = pPersonAliasObject.person.slug
         self.name = pPersonAliasObject.name
-        self.type = "Alternative name for %s" % pPersonAliasObject.person.name 
+        self.type = "Alternative name for %s" % pPersonAliasObject.person.name
         self.title = pPersonAliasObject.person.name
-        
+
 
 def search(request):
     """
@@ -79,7 +81,7 @@ def search(request):
     """
     if not request.GET:
         raise Http404()
-    
+
     try:
         lSearchCriteria = request.GET['q']
     except MultiValueDictKeyError:
@@ -88,42 +90,42 @@ def search(request):
         return render_auth(request, 'search/results_blank_search.html', {"SearchCriteria" : lSearchCriteria})
     lResults = []
     lSearchCriteria = lSearchCriteria.strip()
-    
+
     lBands = Band.objects.filter(name__icontains=lSearchCriteria)
     for band in lBands:
         lType = 'Band'
         if band.status != None:
             lType += ' (%s)' % band.get_status_display()
         lResults.append(ResultWrapper(band,'band', pTypeDisplayText=lType, pName=band.name, pSuffix=band.date_range_display))
-         
+
     lContestGroups = ContestGroup.objects.filter(name__icontains=lSearchCriteria)
     for contestGroup in lContestGroups:
         lResults.append(ResultWrapper(contestGroup,'contest', pTypeDisplayText="Contest Group", pUpperSlug=True))
-        
+
     lContestGroupAliases = ContestGroupAlias.objects.filter(name__icontains=lSearchCriteria)
     for groupAlias in lContestGroupAliases:
         lResults.append(ResultWrapper(groupAlias, 'contest', pTypeDisplayText="Contest Group Alias for %s" % groupAlias.group.name, pUpperSlug=True))
-        
+
     lContests = Contest.objects.filter(name__icontains=lSearchCriteria)
     for contest in lContests:
         lResults.append(ResultWrapper(contest,'contest'))
-        
+
     lContestAliases = ContestAlias.objects.filter(name__icontains=lSearchCriteria)
     for contestAlias in lContestAliases:
         lResults.append(ResultWrapper(contestAlias, 'contest', pTypeDisplayText="Contest Alias for %s" % contestAlias.contest.name))
-        
+
     lTestPieces = TestPiece.objects.filter(name__icontains=lSearchCriteria)
     for piece in lTestPieces:
         lResults.append(ResultWrapper(piece,'piece'))
-        
+
     lTestPieceAliases = TestPieceAlias.objects.filter(name__icontains=lSearchCriteria)
     for pieceAlias in lTestPieceAliases:
         lResults.append(ResultWrapper(pieceAlias, 'piece', pTypeDisplayText="Test Piece Alias for %s" % pieceAlias.piece.name))
-        
+
     lPeople = Person.objects.filter(combined_name__icontains=lSearchCriteria)
     for person in lPeople:
         lResults.append(ResultWrapper(person,'person', pOffset='people', pSuffix=person.bandname))
-        
+
     lPeopleAliases = PersonAlias.objects.filter(name__icontains=lSearchCriteria)
     for person_alias in lPeopleAliases:
         lResults.append(PersonAliasWrapper(person_alias))
@@ -131,19 +133,19 @@ def search(request):
     lPreviousBandNames = PreviousBandName.objects.filter(old_name__icontains=lSearchCriteria).select_related()
     for previous_name in lPreviousBandNames:
         lResults.append(PreviousBandWrapper(previous_name))
-        
+
     lVenues = Venue.objects.filter(name__icontains=lSearchCriteria)
     for venue in lVenues:
         lResults.append(ResultWrapper(venue, 'venue'))
-        
+
     lVenueAliases = VenueAlias.objects.filter(name__icontains=lSearchCriteria)
     for venue_alias in lVenueAliases:
         lResults.append(ResultWrapper(venue_alias.venue, 'venue', pTypeDisplayText='Other venue name', pName=venue_alias.name))
-        
+
     lContestTags = ContestTag.objects.filter(name__icontains=lSearchCriteria)
     for tag in lContestTags:
         lResults.append(ResultWrapper(tag, 'tag'))
-    
+
     return render_auth(request, 'search/results.html', {"SearchCriteria" : lSearchCriteria,
                                                         "SearchResults" : lResults,
                                                         })
