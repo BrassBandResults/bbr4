e679999f30f64f48efda1faebbbcfde0b4294f0e
Tim Sawyer <tim.git@hotmandrins.co.uk>
2022-08-20 12:57:08 +0100

    Correct change password view

--- web/site/bbr/urls.py
+++ web/site/bbr/urls.py
@@ -74,7 +74,7 @@ urlpatterns = [
     re_path(r'^accounts/forgottenpassword/$', user_views.forgotten_password),
     re_path(r'^accounts/forgottenpassword/sent/$', user_views.forgotten_password_sent),
     re_path(r'^accounts/resetpassword/([A-Za-z0-9]+)/$', user_views.reset_password),
-    re_path(r'^accounts/changepassword/$', PasswordChangeView.as_view(template_name='users/changepassword.html', post_change_redirect='/')),
+    re_path(r'^accounts/changepassword/$', PasswordChangeView.as_view(template_name='users/changepassword.html')),
 
     re_path(r'^accounts/loginpro/$', LoginView.as_view(), {'template_name' : 'accounts/loginpro.html',}),
     re_path(r'^accounts/upgrade/$', user_views.pro_upgrade),
