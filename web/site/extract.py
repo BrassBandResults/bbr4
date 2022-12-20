361bea76449e14bb819d21fce7af6cb4528521ce
Tim Sawyer <tim.git@hotmandrins.co.uk>
2022-12-20 10:22:54 +0000

    Extract improvements

--- web/site/extract.py
+++ web/site/extract.py
@@ -1,7 +1,7 @@
 #!/home/bbr/.venv/bbr4/bin/python
 # (c) 2012, 2018 Tim Sawyer, All Rights Reserved
 
-import sys, os, re, time
+import sys, os, re, time, shutil
 from datetime import datetime, timedelta
 from os.path import expanduser
 
@@ -13,7 +13,7 @@ django.setup()
 from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 
-from contests.models import ContestEvent
+from contests.models import ContestEvent, Venue
 from bands.models import Band
 from people.models import Person
 from pieces.models import TestPiece
@@ -28,59 +28,50 @@ def write_file(filepath, filename, contents):
 	f.write(contents)
 	f.close()
 
-#print ("Extracting %d Contest Events" % ContestEvent.objects.count())
-#lAllEvents = ContestEvent.objects.all().order_by('-date_of_event')
-#lYear = None
-#for event in lAllEvents:
-#	if lYear != event.event_year:
-#		print (lYear)
-#		lYear = event.event_year
-#	print ("\t%s - %s" % (event.date_of_event, event.name))
-#
-#	lFilepath = "%s/Contest Events/%d/%d/%d" % (HOME, event.date_of_event.year, event.date_of_event.month, event.date_of_event.day)
-#	lFilename = "%s.xml" % event.name
-#	lContestXml = render_to_string('extract/contest_event.xml', { 'ContestEvent' : event, })
-#	write_file(lFilepath, lFilename, lContestXml)
-#	time.sleep(0.2)
+def extract(list, dir, template, templateVar):
+	print ("Extracting %d %s" % list.count(), dir)
+	shutil.rmtree(HOME + "/" + dir)
+	lAllRows = list.all().order_by('name')
+	for each in lAllRows:
+		print ("\t%s" % each.name)
+		try:
+			lDir = each.slug[0:1]
+		except IndexError:
+			lDir = "_"
+		lFilepath = "%s/%s/%s" % (HOME, dir, lDir)
+		lFilename = "%s.xml" % each.slug
+		lXml = render_to_string('extract/%s.xml' % template, { templateVar : each, })
+		write_file(lFilepath, lFilename, lXml)
+		time.sleep(0.1)
 
-print ("Extracting %d Bands" % Band.objects.count())
-lAllBands = Band.objects.all().order_by('name')
-for band in lAllBands:
-	print ("\t%s" % band.name)
-	try:
-		lDir = band.slug[0:1]
-	except IndexError:
-		lDir = "_"
-	lFilepath = "%s/Bands/%s" % (HOME, lDir)
-	lFilename = "%s.xml" % band.slug
-	lBandXml = render_to_string('extract/band.xml', { 'Band' : band, })
-	write_file(lFilepath, lFilename, lBandXml)
-	time.sleep(0.1)
+typeToGenerate = sys.argv[1]
+print("Extracting with command line parameter" + typeToGenerate)
 
-print ("Extracting %d People" % Person.objects.count())
-lAllPeople = Person.objects.all().order_by('surname')
-for person in lAllPeople:
-	print ("\t%s, %s" % (person.surname, person.first_names))
-	try:
-		lDir = person.slug[0:1]
-	except IndexError:
-		lDir = "_"
-	lFilepath = "%s/People/%s" % (HOME, lDir)
-	lFilename = "%s.xml" % person.slug
-	lPersonXml = render_to_string('extract/person.xml', { 'Person' : person, })
-	write_file(lFilepath, lFilename, lPersonXml)
-	time.sleep(0.1)
+if typeToGenerate == "results":
+	print ("Extracting %d Contest Events" % ContestEvent.objects.count())
+	shutil.rmtree(HOME + "/Contest Events")
+	lAllEvents = ContestEvent.objects.all().order_by('-date_of_event')
+	lYear = None
+	for event in lAllEvents:
+		if lYear != event.event_year:
+			print (lYear)
+			lYear = event.event_year
+		print ("\t%s - %s" % (event.date_of_event, event.name))
 
-print ("Extracting %d Pieces" % TestPiece.objects.count())
-lAllPieces = TestPiece.objects.all().order_by('name')
-for piece in lAllPieces:
-	print ("\t%s" % piece.name)
-	try:
-		lDir = piece.slug[0:1]
-	except IndexError:
-		lDir = "_"
-	lFilepath = "%s/Pieces/%s" % (HOME, lDir)
-	lFilename = "%s.xml" % person.slug
-	lPieceXml = render_to_string('extract/piece.xml', { 'Piece' : piece, })
-	write_file(lFilepath, lFilename, lPieceXml)
-	time.sleep(0.1)	
+		lFilepath = "%s/Contest Events/%d/%d/%d" % (HOME, event.date_of_event.year, event.date_of_event.month, event.date_of_event.day)
+		lFilename = "%s.xml" % event.name
+		lContestXml = render_to_string('extract/contest_event.xml', { 'ContestEvent' : event, })
+		write_file(lFilepath, lFilename, lContestXml)
+		time.sleep(0.1)
+
+elif typeToGenerate == "bands":
+	extract(Band.objects, "Bands", 'band', 'Band')
+
+elif typeToGenerate == "people":
+	extract(Person.objects, "People", 'person', 'Person')
+
+elif typeToGenerate == "pieces":
+	extract(TestPiece.objects, "Pieces", 'piece', 'Piece')
+
+elif typeToGenerate == "venues":
+	extract(Venue.objects, "Venues", 'venue', 'Venue')
