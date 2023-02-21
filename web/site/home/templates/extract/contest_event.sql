fe2952c7a96dd270305c955aa85bc7ed0949c11f
Tim Sawyer <tim.git@hotmandrins.co.uk>
2023-02-21 22:11:09 +0000

    Initial event sql code

--- web/site/home/templates/extract/contest_event.sql
+++ web/site/home/templates/extract/contest_event.sql
@@ -1,6 +1,6 @@
 INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, name, date_of_event, date_resolution, contest_id, notes, venue_id, complete, no_contest, contest_type_id, original_owner)
   VALUES (
-    {ContestEvent.id}},
+    {{ContestEvent.id}},
     CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
     CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
     (SELECT id FROM user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
@@ -14,9 +14,9 @@ INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, na
     {% if ContestEvent.complete %}1{%else%}0{%endif%},
     {% if ContestEvent.no_contest %}1{%else%}0{%endif%},
 {% if ContestEvent.contest_type_override_link %}
-  (SELECT if FROM contest WHERE old_id = {{ContestEvent.contest_type_override_link.id}}),
+  (SELECT id FROM contest WHERE old_id = {{ContestEvent.contest_type_override_link.id}}),
 {% else %}
-  (SELECT if FROM contest WHERE old_id = {{ContestEvent.contest.id}}),
+  (SELECT id FROM contest WHERE old_id = {{ContestEvent.contest.id}}),
 {% endif %}
     {{ContestEvent.owner.username}}
   );
@@ -24,11 +24,11 @@ INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, na
 {% if ContestEvent.test_piece %}
 INSERT INTO contest_event_test_piece (updated, created, updated_by_id, owner_id, contest_event_id, piece_id, and_or)
 VALUES (
-  CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
+    CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
     CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
     (SELECT id FROM user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
     (SELECT id FROM user WHERE usercode='{{ContestEvent.contest.lastChangedBy.username}}'),
-    (SELECT id FROM contest_event WHERE old_id = {ContestEvent.id}}),
+    (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     {{ContestEvent.test_piece.id}},
     null
 );
@@ -41,7 +41,7 @@ VALUES (
     CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
     (SELECT id FROM user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
     (SELECT id FROM user WHERE usercode='{{ContestEvent.contest.lastChangedBy.username}}'),
-    (SELECT id FROM contest_event WHERE old_id = {ContestEvent.id}}),
+    (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     {{piece.test_piece.id}},
     {{piece.and_or}}
 );
@@ -58,11 +58,15 @@ VALUES (
   (SELECT id FROM user WHERE usercode='{{result.lastChangedBy.username}}'),
   (SELECT id FROM contest_event WHERE id={{CcontestEvent.id}}),
   {{result.band.id}},
-  {{result.band_name}},
+  '{{result.band_name}}',
   '{{result.results_position_display}}',
   '{{result.results_position_display}}',
   {{result.draw}},
+{% if result.draw_second_part %}
   {{result.draw_second_part}},
+{% else %}
+  null,
+{% end if %}
   '{{result.points}}',
   '{{result.points_first_part}}',
   '{{result.points_second_part}}',
@@ -70,13 +74,13 @@ VALUES (
   '{{result.points_fourth_part}}',
   '{{result.penalty_points}}',
   '{{result.conductor_name}}',
-  (SELECT id FROM person WHERE old_id={{result.second_person_conducting.id}}),
+  (SELECT id FROM person WHERE old_id={{result.person_conducting.id}}),
   {% if result.second_person_conducting %}
   (SELECT id FROM person WHERE old_id={{result.second_person_conducting.id}}),
   {% else %}
   null,
   {% endif %}
-  {{result.notes}}
+  '{{result.notes}}'
 );
 
 {% if result.test_piece %}
@@ -90,6 +94,7 @@ VALUES (
   {{test_piece.id}},
   0,
   null
+);
 {% endif %}
 
 {% for extra_piece in result.resultpieceperformance_set.all %}
@@ -102,7 +107,7 @@ VALUES (
   {{result.id}},
   {{extra_piece.piece.id}},
   {{extra_piece.ordering}},
-  {{extra_piece.suffix}}
+  '{{extra_piece.suffix}}'
 );
 {% endfor %}
 
