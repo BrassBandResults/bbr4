94efd7bcaf10176a396c2004956c66ba64a4347e
Tim Sawyer <tim.git@hotmandrins.co.uk>
2023-03-04 15:24:38 +0000

    Fixes for results generation

--- web/site/home/templates/extract/contest_event.sql
+++ web/site/home/templates/extract/contest_event.sql
@@ -1,33 +1,33 @@
 INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, name, date_of_event, date_resolution, contest_id, notes, venue_id, complete, no_contest, contest_type_id, original_owner)
   VALUES (
     {{ContestEvent.id}},
-    CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
-    CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.lastChangedBy.username}}'),
+    CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
+    CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
     '{{ContestEvent.name}}',
     CONVERT(date, '{{ContestEvent.date_of_event.day}}-{{ContestEvent.date_of_event.month}}-{{ContestEvent.date_of_event.year}}', 110),
     '{{ContestEvent.date_resolution}}',
     {{ContestEvent.contest.id}},
-    '{{ContestEvent.contest.notes}}',
+    '{{ContestEvent.notes}}',
     {% if ContestEvent.venue_link %}(SELECT id FROM venue WHERE old_id={{ContestEvent.venue_link.id}}){% else %}null{% endif %},
     {% if ContestEvent.complete %}1{%else%}0{%endif%},
     {% if ContestEvent.no_contest %}1{%else%}0{%endif%},
 {% if ContestEvent.contest_type_override_link %}
   (SELECT id FROM contest_type WHERE old_id = {{ContestEvent.contest_type_override_link.id}}),
 {% else %}
-  (SELECT id FROM contest_type WHERE old_id = {{ContestEvent.contest.id}}),
+  (SELECT id FROM contest_type WHERE old_id = {{ContestEvent.contest.contest_type_link.id}}),
 {% endif %}
-    '{{ContestEvent.owner.username}}'
+    '{{ContestEvent.original_owner.username}}'
   );
 
 {% if ContestEvent.test_piece %}
 INSERT INTO contest_event_test_piece (updated, created, updated_by_id, owner_id, contest_event_id, piece_id, and_or)
 VALUES (
-    CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
-    CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.lastChangedBy.username}}'),
+    CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
+    CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
     (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     {{ContestEvent.test_piece.id}},
     null
@@ -37,13 +37,18 @@ VALUES (
 {% for piece in ContestEvent.contesttestpiece_set.all %}
 INSERT INTO contest_event_test_piece (updated, created, updated_by_id, owner_id, contest_event_id, piece_id, and_or)
 VALUES (
-    CONVERT(datetime, '{{ContestEvent.contest.last_modified|date:"Y-m-d H:i:s"}}', 20),
-    CONVERT(datetime, '{{ContestEvent.contest.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.contest.lastChangedBy.username}}'),
+    CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
+    CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
+    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
     (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     {{piece.test_piece.id}},
-    {{piece.and_or}}
+    {% if piece.and_or == 'and' %}
+    'A'
+    {% else if piece.and_or == 'or' %}
+    'O'
+    {%% else %}
+    null
 );
 {% endfor %}
 
@@ -106,8 +111,8 @@ VALUES (
   CONVERT(datetime, '{{result.created|date:"Y-m-d H:i:s"}}', 20),
   (SELECT id FROM site_user WHERE usercode='{{result.owner.username}}'),
   (SELECT id FROM site_user WHERE usercode='{{result.lastChangedBy.username}}'),
-  {{result.id}},
-  {{test_piece.id}},
+  (SELECT id FROM contest_result WHERE old_id = {{result.id}}),
+  (SELECT id FROM piece WHERE old_id = {{test_piece.id}}),
   0,
   null
 );
@@ -120,8 +125,8 @@ VALUES (
   CONVERT(datetime, '{{result.created|date:"Y-m-d H:i:s"}}', 20),
   (SELECT id FROM site_user WHERE usercode='{{result.owner.username}}'),
   (SELECT id FROM site_user WHERE usercode='{{result.lastChangedBy.username}}'),
-  {{result.id}},
-  {{extra_piece.piece.id}},
+  (SELECT id FROM contest_result WHERE old_id = {{result.id}}),
+  (SELECT id FROM piece WHERE old_id = {{extra_piece.piece.id}}),
   {{extra_piece.ordering}},
   '{{extra_piece.suffix}}'
 );
