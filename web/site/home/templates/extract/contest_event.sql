3a916b1e79ef188aba47272277b653bb393fe487
Tim Sawyer <tim.git@hotmandrins.co.uk>
2023-03-25 17:20:38 +0000

    New style owner/updated fields

--- web/site/home/templates/extract/contest_event.sql
+++ web/site/home/templates/extract/contest_event.sql
@@ -1,10 +1,10 @@
-INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, name, date_of_event, date_resolution, contest_id, notes, venue_id, complete, no_contest, contest_type_id, original_owner)
+INSERT INTO contest_event (old_id, updated, created, updated_by, created_by, name, date_of_event, date_resolution, contest_id, notes, venue_id, complete, no_contest, contest_type_id, original_owner)
   VALUES (
     {{ContestEvent.id}},
     CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
     CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
+    '{{ContestEvent.owner.username}}',
+    '{{ContestEvent.lastChangedBy.username}}',
     '{{ContestEvent.name_migrate|safe}}',
     CONVERT(date, '{{ContestEvent.date_of_event.day}}-{{ContestEvent.date_of_event.month}}-{{ContestEvent.date_of_event.year}}', 105),
     '{{ContestEvent.date_resolution}}',
@@ -22,12 +22,12 @@ INSERT INTO contest_event (old_id, updated, created, updated_by_id, owner_id, na
   );
 
 {% if ContestEvent.test_piece %}
-INSERT INTO contest_event_test_piece (updated, created, updated_by_id, owner_id, contest_event_id, piece_id, and_or)
+INSERT INTO contest_event_test_piece (updated, created, updated_by, created_by, contest_event_id, piece_id, and_or)
 VALUES (
     CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
     CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
+    '{{ContestEvent.owner.username}}',
+    '{{ContestEvent.lastChangedBy.username}}',
     (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     (SELECT id FROM piece WHERE old_id={{ContestEvent.test_piece.id}}),
     null
@@ -35,12 +35,12 @@ VALUES (
 {%endif%}
 
 {% for piece in ContestEvent.contesttestpiece_set.all %}
-INSERT INTO contest_event_test_piece (updated, created, updated_by_id, owner_id, contest_event_id, piece_id, and_or)
+INSERT INTO contest_event_test_piece (updated, created, updated_by, created_by, contest_event_id, piece_id, and_or)
 VALUES (
     CONVERT(datetime, '{{ContestEvent.last_modified|date:"Y-m-d H:i:s"}}', 20),
     CONVERT(datetime, '{{ContestEvent.created|date:"Y-m-d H:i:s"}}', 20),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.owner.username}}'),
-    (SELECT id FROM site_user WHERE usercode='{{ContestEvent.lastChangedBy.username}}'),
+    '{{ContestEvent.owner.username}}',
+    '{{ContestEvent.lastChangedBy.username}}',
     (SELECT id FROM contest_event WHERE old_id = {{ContestEvent.id}}),
     (SELECT id FROM piece WHERE old_id={{piece.test_piece.id}}),
     {% if piece.and_or == 'and' %}
@@ -54,14 +54,14 @@ VALUES (
 {% endfor %}
 
 {% for result in ContestEvent.contestresult_set.all %}
-INSERT INTO contest_result (old_id, updated, created, updated_by_id, owner_id, contest_event_id, band_id, band_name, result_position_type, result_position, draw, draw_second, 
+INSERT INTO contest_result (old_id, updated, created, updated_by, created_by, contest_event_id, band_id, band_name, result_position_type, result_position, draw, draw_second, 
                             points_total, points_first, points_second, points_third, points_fourth, points_penalty, conductor_name, conductor_id, conductor_two_id, notes)
 VALUES (
   {{result.id}},
   CONVERT(datetime, '{{result.last_modified|date:"Y-m-d H:i:s"}}', 20),
   CONVERT(datetime, '{{result.created|date:"Y-m-d H:i:s"}}', 20),
-  (SELECT id FROM site_user WHERE usercode='{{result.owner.username}}'),
-  (SELECT id FROM site_user WHERE usercode='{{result.lastChangedBy.username}}'),
+  '{{result.owner.username}}',
+  '{{result.lastChangedBy.username}}',
   (SELECT id FROM contest_event WHERE old_id={{ContestEvent.id}}),
   (SELECT id FROM band WHERE old_id={{result.band.id}}),
   '{{result.band_name_export|safe}}',
@@ -106,12 +106,12 @@ VALUES (
 );
 
 {% if result.test_piece %}
-INSERT INTO contest_result_test_piece (updated, created, updated_by_id, owner_id, contest_result_id, piece_id, ordering, suffix)
+INSERT INTO contest_result_test_piece (updated, created, updated_by, created_by, contest_result_id, piece_id, ordering, suffix)
 VALUES (
   CONVERT(datetime, '{{result.last_modified|date:"Y-m-d H:i:s"}}', 20),
   CONVERT(datetime, '{{result.created|date:"Y-m-d H:i:s"}}', 20),
-  (SELECT id FROM site_user WHERE usercode='{{result.owner.username}}'),
-  (SELECT id FROM site_user WHERE usercode='{{result.lastChangedBy.username}}'),
+  '{{result.owner.username}}',
+  '{{result.lastChangedBy.username}}',
   (SELECT id FROM contest_result WHERE old_id = {{result.id}}),
   (SELECT id FROM piece WHERE old_id = {{result.test_piece.id}}),
   0,
@@ -120,12 +120,12 @@ VALUES (
 {% endif %}
 
 {% for extra_piece in result.resultpieceperformance_set.all %}
-INSERT INTO contest_result_test_piece (updated, created, updated_by_id, owner_id, contest_result_id, piece_id, ordering, suffix)
+INSERT INTO contest_result_test_piece (updated, created, updated_by, created_by, contest_result_id, piece_id, ordering, suffix)
 VALUES (
   CONVERT(datetime, '{{result.last_modified|date:"Y-m-d H:i:s"}}', 20),
   CONVERT(datetime, '{{result.created|date:"Y-m-d H:i:s"}}', 20),
-  (SELECT id FROM site_user WHERE usercode='{{result.owner.username}}'),
-  (SELECT id FROM site_user WHERE usercode='{{result.lastChangedBy.username}}'),
+  '{{result.owner.username}}',
+  '{{result.lastChangedBy.username}}',
   (SELECT id FROM contest_result WHERE old_id = {{result.id}}),
   (SELECT id FROM piece WHERE old_id = {{extra_piece.piece.id}}),
   {{extra_piece.ordering}},
